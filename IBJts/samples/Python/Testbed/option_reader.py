d0326e3c08ca35e9cfa7d390e2820ff65cad2ab3
dmitryhits <dmitry.hits@gmail.com>
2018-02-14 20:49:16 +0100

    "adding option processing tool"

---
+++ IBJts/samples/Python/Testbed/option_reader.py
@@ -0,0 +1,22 @@
+"""
+reads files one by one for each date,
+plots the underlying prices
+determines the sigma of an underlying symbol
+extracts option chain within x*sigma around the underlying value
+"""
+
+import glob
+import pandas as pd
+import numpy as np
+
+main_path = "/Users/hits/Desktop/livediscount2003_optiondatabucketzip/"
+files = glob.glob(main_path+'**/*csv', recursive=True)
+bank_days = {file.strip('_OData.csv').split('/')[-1]:file for file in files}
+
+apple_price = []
+for day in bank_days:
+    option_sample = pd.read_csv(bank_days[day], index_col='Symbol')
+    apple_price.append(option_sample['UnderlyingPrice'].loc['AAPL'].unique()[0])
+
+apple_price = pd.Series(apple_price)
+print(apple_price.mean(), apple_price.std())
